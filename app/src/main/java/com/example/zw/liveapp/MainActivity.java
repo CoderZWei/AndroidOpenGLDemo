bc30490840bf0a8e261f1a33af55cdf0e3c79db8
zw <1622993630@qq.com>
2019-05-17 00:23:19 +0800

    测试

--- app/src/main/java/com/example/zw/liveapp/MainActivity.java
+++ app/src/main/java/com/example/zw/liveapp/MainActivity.java
@@ -5,20 +5,12 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //测试
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
     }
 
     /**
