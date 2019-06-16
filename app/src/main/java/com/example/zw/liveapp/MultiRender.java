f8009199ccff623fface4d0185a12b09feaad6f5
zw <1622993630@qq.com>
2019-06-16 15:48:05 +0800

    单Surface渲染多个纹理1

--- app/src/main/java/com/example/zw/liveapp/MultiRender.java
+++ app/src/main/java/com/example/zw/liveapp/MultiRender.java
@@ -1,7 +1,10 @@
 package com.example.zw.liveapp;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.opengl.GLES20;
+import android.opengl.GLUtils;
 
 import com.example.zw.liveapp.utils.ShaderUtil;
 
@@ -16,7 +19,11 @@ public class MultiRender implements MyEGLSurfaceView.MyGLRender
             -1f, -1f,
             1f, -1f,
             -1f, 1f,
-            1f, 1f
+            1f, 1f,
+            //第二块纹理 在其上绘制一块三角形
+            -0.25f, -0.25f,
+            0.25f, -0.25f,
+            0f, 0.15f
     };
     private FloatBuffer vertexBuffer;
     //fbo有自己的坐标系，需要单独设置
@@ -41,6 +48,7 @@ public class MultiRender implements MyEGLSurfaceView.MyGLRender
     private int vPosition;
     private int fPosition;
     private int textureId;
+    private int migTextureId;
     private int sampler;
     //顶点缓冲
     private int vboId;
@@ -92,6 +100,8 @@ public class MultiRender implements MyEGLSurfaceView.MyGLRender
         GLES20.glBufferSubData(GLES20.GL_ARRAY_BUFFER, 0, vertexData.length * 4, vertexBuffer);
         GLES20.glBufferSubData(GLES20.GL_ARRAY_BUFFER, vertexData.length * 4, fragmentData.length * 4, fragmentBuffer);
         GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
+
+        migTextureId=loadTexrute(R.mipmap.img2);
     }
 
     @Override
@@ -103,25 +113,45 @@ public class MultiRender implements MyEGLSurfaceView.MyGLRender
     public void onDrawFrame() {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
         GLES20.glClearColor(1f,0f, 0f, 1f);
-
         GLES20.glUseProgram(program);
-
-        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureId);
-
-
         GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, vboId);
-
+        //
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureId);
         GLES20.glEnableVertexAttribArray(vPosition);
         GLES20.glVertexAttribPointer(vPosition, 2, GLES20.GL_FLOAT, false, 8,
                 0);
-
+        GLES20.glEnableVertexAttribArray(fPosition);
+        GLES20.glVertexAttribPointer(fPosition, 2, GLES20.GL_FLOAT, false, 8,
+                vertexData.length * 4);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
+        //第二张纹理
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, migTextureId);
+        GLES20.glEnableVertexAttribArray(vPosition);
+        GLES20.glVertexAttribPointer(vPosition, 2, GLES20.GL_FLOAT, false, 8,
+                32);
         GLES20.glEnableVertexAttribArray(fPosition);
         GLES20.glVertexAttribPointer(fPosition, 2, GLES20.GL_FLOAT, false, 8,
                 vertexData.length * 4);
 
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
-
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 3);
+        //
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
         GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
+    }
+    private int loadTexrute(int src){
+        int []textureIds = new int[1];
+        GLES20.glGenTextures(1, textureIds, 0);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureIds[0]);
+
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_REPEAT);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_REPEAT);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(mContext.getResources(), src);
+        GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0);
+
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
+        return  textureIds[0];
     }
 }
